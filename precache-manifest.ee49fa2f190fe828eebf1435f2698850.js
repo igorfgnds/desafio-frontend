4440eb972c7b142404f26b7cb8018da4bd5c1f73
Igor Fagundes <igorfgnds@gmail.com>
2019-06-25 21:46:00 -0300

    Updates

---
+++ precache-manifest.ee49fa2f190fe828eebf1435f2698850.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebc6c9918e29f64b9423dac49bd889d8",
+    "url": "/silveira-barreiros/index.html"
+  },
+  {
+    "revision": "a5431133856a8066c486",
+    "url": "/silveira-barreiros/static/css/main.e1b27846.chunk.css"
+  },
+  {
+    "revision": "9ee7da7090cf36a48498",
+    "url": "/silveira-barreiros/static/js/2.2edad468.chunk.js"
+  },
+  {
+    "revision": "a5431133856a8066c486",
+    "url": "/silveira-barreiros/static/js/main.a2965c68.chunk.js"
+  },
+  {
+    "revision": "02654bb552a9c15cfe9e",
+    "url": "/silveira-barreiros/static/js/runtime~main.2b1be150.js"
+  }
+]);
